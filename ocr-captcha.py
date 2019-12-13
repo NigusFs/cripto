bfdbc736fcc37bd3a90c2c674eb2ce336b1ee1de
Nicolas Flores <nicolas.floress@mail.udp.cl>
2019-12-13 03:12:06 -0300

    Final ocr-captcha
    This py file recieve an url of a captcha's image and solve it with pytesseract

--- ocr-captcha.py
+++ ocr-captcha.py
@@ -1,28 +1,23 @@
-#import matplotlib.pyplot as plt
+#Python 3.6.7
+#reference https://pypi.org/project/pytesseract/
+#by NFS
 import urllib
 from urllib.request import urlopen
 from PIL import Image
 import pytesseract
 import re
+import sys
 
-pytesseract.pytesseract.tesseract_cmd =r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+#pytesseract.pytesseract.tesseract_cmd =r'C:\Program Files\Tesseract-OCR\tesseract.exe' #it is useful with windows
 
-url='https://www.gsctx.org/en/our-council/web-to-case.html'
+url_burp=str(sys.argv[1]) #url from burp suite
 
-response = urlopen(url)
-html = response.read().decode('utf-8')
+image = Image.open(urllib.request.urlopen(url_burp))
+captcha=pytesseract.image_to_string(image) #ocr
 
-url_captcha = re.findall(r'<img id=\"cq_captchaimg\" src=\"/content/gsctx/en/our-council/web-to-case/_jcr_content/content/middle/par/captcha\.captcha\.png\?id=123\" alt=\"\"></div>', html)[0]
-id_captcha=re.findall(r'[0-9]+',str(url_captcha))
-#print(id_captcha[0])
-
-url_captcha = 'https://www.gsctx.org/en/our-council/web-to-case/jcr:content/content/middle/par/captcha.captcha.png?id='+str(id_captcha[0])
-image = Image.open(urllib.request.urlopen(url_captcha))
-
-
-captcha=pytesseract.image_to_string(image)
-print("Request\n",url)
-print("Response\n Captcha CODE: ",captcha)
+print("Request")
+print(url_burp)
+print("Response\nCaptcha CODE: ",captcha)
 
 #plt.imshow(image)
 #plt.show()
