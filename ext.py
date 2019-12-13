eb49d250533ae38397c202d6bbe4d784696e1b57
Nicolas Flores <nicolas.floress@mail.udp.cl>
2019-12-13 03:09:30 -0300

    Final anti-captcha extension for burp suite
    This py file get a captcha's url  from burp suite and  it send to a ocr-captcha.py to solve the captcha

--- ext.py
+++ ext.py
@@ -1,19 +1,31 @@
+#jython-standalone-2.7.1
+#reference https://github.com/PortSwigger/example-event-listeners
+#by NFS
+
 from burp import IBurpExtender
+from burp import IHttpListener
+from burp import IExtensionStateListener
 from java.io import PrintWriter
-from java.lang import RuntimeException
 import sys, os
-class BurpExtender(IBurpExtender):
-    
-    
-    def	registerExtenderCallbacks(self, callbacks):
-               
+import re
 
-        stdout = PrintWriter(callbacks.getStdout(), True)
-        stderr = PrintWriter(callbacks.getStderr(), True)
-       
-       	ocr_captcha=os.popen('python ocr-captcha.py').read()
-        stdout.println(ocr_captcha) #print in console
+class BurpExtender(IBurpExtender, IHttpListener):
 
-        stderr.println("errors")
-        callbacks.issueAlert("alerts")
-        raise RuntimeException("exception")
+    def	registerExtenderCallbacks(self, callbacks): #something to create an extension
+        self._callbacks = callbacks
+        callbacks.setExtensionName("Anti-captcha")
+        self._stdout = PrintWriter(callbacks.getStdout(), True)
+        callbacks.registerHttpListener(self)
+      
+
+    def processHttpMessage(self, toolFlag, messageIsRequest, RequestInfo):
+        if  RequestInfo.getHttpService().toString()=="https://www.gsctx.org" and messageIsRequest: #condition to recieve get of gsctx.org
+
+            aux=RequestInfo.getUrl().toString() #get the url of the request/message
+            url_captcha=re.findall(r'https://www\.gsctx\.org:443/en/our-council/web-to-case/jcr:content/content/middle/par/captcha\.captcha\.png\?id=[0-9]+',str(aux))
+            #^- filter to send url's captchas
+            if len(url_captcha) >0:
+                ocr_captcha=os.popen('python ocr-captcha.py {}'.format(url_captcha[0])).read() # this is to avoid import problems
+                self._stdout.println(ocr_captcha)
+
+
